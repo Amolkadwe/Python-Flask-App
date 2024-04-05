9f6cfa0c1f3ba2ad16ce660de53e141966a195b7
Amol Kadwe <70431768+Amolkadwe@users.noreply.github.com>
2024-04-05 10:58:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask On K8s!! Automated From Jenkins"
+    return "Flask On K8s!! Automated From Jenkins Version-1"
 
 
 if __name__ == "__main__":
