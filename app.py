c4ce85369f058f3cc47640ef25c6fa0200dafbef
Amol Kadwe <70431768+Amolkadwe@users.noreply.github.com>
2024-04-13 17:50:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask App Deployed Throgh Ansible v2 "
+    return "Flask App Deployed Throgh Ansible Version-1 "
 
 
 if __name__ == "__main__":
